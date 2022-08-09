cb716939635d7fcaf3a54ad79246d2e7275f19f8
SG1515 <sunggu507@naver.com>
2022-08-09 22:43:12 +0900

    운영용 db 접속정보 추가, 톰캣7메이븐 플러그인 배포 설정

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -1,10 +1,26 @@
 package com.ll.exam;
 
 public class App {
-    public static final String BASE_PACKAGE_PATH = "com.ll.exam";
-    public static final String DB_HOST = "localhost";
-    public static final int DB_PORT = 3306;
-    public static final String DB_ID = "sg";
-    public static final String DB_PASSWORD = "12341234";
-    public static final String DB_NAME = "blog";
+    public static String DB_HOST = "localhost";
+    public static int DB_PORT = 3306;
+    public static String DB_ID = "sg";
+    public static String DB_PASSWORD = "12341234";
+    public static String DB_NAME = "blog";
+    public static boolean isProd = false;
+
+    static {
+        String profilesActive = System.getenv("SPRING_PROFILES_ACTIVE");
+
+        if (profilesActive != null && profilesActive.equals("production")) {
+            isProd = true;
+        }
+
+        if (isProd) {
+            DB_HOST = "172.17.0.1";
+            DB_PORT = 3306;
+            DB_ID = "sg";
+            DB_PASSWORD = "12341234";
+            DB_NAME = "site1";
+        }
+    }
 }
